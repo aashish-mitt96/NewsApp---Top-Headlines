3ed1c737aa6b2223a3ba6c89eb09bdc65279c836
Ashish Mittal <aashishrbmittal@gmail.com>
2025-04-04 18:50:20 +0530

    final

--- server/api/news.js
+++ server/api/news.js
@@ -1,24 +1,17 @@
-const express = require("express");
 const axios = require("axios");
-const cors = require("cors");
 
-const app = express();
-app.use(cors());
-
-app.get("/api/news", async (req, res) => {
+module.exports = async (req, res) => {
   try {
     const apiKey = process.env.NEWS_API_KEY;
     const pageSize = req.query.pageSize || 9;
     const page = req.query.page || 1;
 
     const apiUrl = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=${apiKey}&pageSize=${pageSize}&page=${page}`;
-
     const response = await axios.get(apiUrl);
-    res.json(response.data);
+
+    res.status(200).json(response.data);
   } catch (error) {
-    console.error("Error fetching news:", error);
+    console.error("Error fetching news:", error.message);
     res.status(500).json({ error: "Failed to fetch news" });
   }
-});
-
-module.exports = app;
+};
